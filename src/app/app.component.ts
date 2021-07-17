3e3182f220b46368bbeed8c2d5236230425a502b
dweizhe <d.weizhe@anyong.com.tw>
2021-07-18 01:45:23 +0800

    first version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,440 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+type Award = {
+  name: string;
+  probability: number;
+  goal: boolean;
+};
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'lottery';
+export class AppComponent implements OnInit {
+  public fakeNames = [
+    '妙蛙種子',
+    '妙蛙草',
+    '妙蛙花',
+    '小火龍',
+    '火恐龍',
+    '噴火龍',
+    '傑尼龜',
+    '卡咪龜',
+    '水箭龜',
+    '綠毛蟲',
+    '鐵甲蛹',
+    '巴大蝶',
+    '獨角蟲',
+    '鐵殼蛹',
+    '大針蜂',
+    '波波',
+    '比比鳥',
+    '大比鳥',
+    '小拉達',
+    '拉達',
+    '烈雀',
+    '大嘴雀',
+    '阿柏蛇',
+    '阿柏怪',
+    '皮卡丘',
+    '雷丘',
+    '穿山鼠',
+    '穿山王',
+    '尼多蘭',
+    '尼多娜',
+    '尼多后',
+    '尼多朗',
+    '尼多力諾',
+    '尼多王',
+    '皮皮',
+    '皮可西',
+    '六尾',
+    '九尾',
+    '胖丁',
+    '胖可丁',
+    '超音蝠',
+    '大嘴蝠',
+    '走路草',
+    '臭臭花',
+    '霸王花',
+    '派拉斯',
+    '派拉斯特',
+    '毛球',
+    '摩魯蛾',
+    '地鼠',
+    '三地鼠',
+    '喵喵',
+    '貓老大',
+    '可達鴨',
+    '哥達鴨',
+    '猴怪',
+    '火爆猴',
+    '卡蒂狗',
+    '風速狗',
+    '蚊香蝌蚪',
+    '蚊香君',
+    '蚊香泳士',
+    '凱西',
+    '勇基拉',
+    '胡地',
+    '腕力',
+    '豪力',
+    '怪力',
+    '喇叭芽',
+    '口呆花',
+    '大食花',
+    '瑪瑙水母',
+    '毒刺水母',
+    '小拳石',
+    '隆隆石',
+    '隆隆岩',
+    '小火馬',
+    '烈焰馬',
+    '呆呆獸',
+    '呆殼獸',
+    '小磁怪',
+    '三合一磁怪',
+    '大蔥鴨',
+    '嘟嘟',
+    '嘟嘟利',
+    '小海獅',
+    '白海獅',
+    '臭泥',
+    '臭臭泥',
+    '大舌貝',
+    '刺甲貝',
+    '鬼斯',
+    '鬼斯通',
+    '耿鬼',
+    '大岩蛇',
+    '催眠貘',
+    '引夢貘人',
+    '大鉗蟹',
+    '巨鉗蟹',
+    '霹靂電球',
+    '頑皮雷彈',
+    '蛋蛋',
+    '椰蛋樹',
+    '卡拉卡拉',
+    '嘎啦嘎啦',
+    '飛腿郎',
+    '快拳郎',
+    '大舌頭',
+    '瓦斯彈',
+    '雙彈瓦斯',
+    '獨角犀牛',
+    '鑽角犀獸',
+    '吉利蛋',
+    '蔓藤怪',
+    '袋獸',
+    '墨海馬',
+    '海刺龍',
+    '角金魚',
+    '金魚王',
+    '海星星',
+    '寶石海星',
+    '魔牆人偶',
+    '飛天螳螂',
+    '迷唇姐',
+    '電擊獸',
+    '鴨嘴火獸',
+    '凱羅斯',
+    '肯泰羅',
+    '鯉魚王',
+    '暴鯉龍',
+    '拉普拉斯',
+    '百變怪',
+    '伊布',
+    '水伊布',
+    '雷伊布',
+    '火伊布',
+    '多邊獸',
+    '菊石獸',
+    '多刺菊石獸',
+    '化石盔',
+    '鐮刀盔',
+    '化石翼龍',
+    '卡比獸',
+    '急凍鳥',
+    '閃電鳥',
+    '火焰鳥',
+    '迷你龍',
+    '哈克龍',
+    '快龍',
+    '超夢',
+    '夢幻',
+  ];
+
+  public sameProbability = true;
+
+  public cost = 60;
+
+  public quantity = 1;
+
+  public awards: Award[] = [];
+
+  public remainingProbability = 100;
+
+  public needCost = 0;
+
+  public needCount = 0;
+
+  public currentAchievementProbability = 0;
+
+  public targetAchievementProbability = 0;
+
+  public step: number = 0;
+
+  public targetAwardList: string = '';
+
+  public series: { name: string; value: number }[] = [];
+
+  public results: {
+    name: string;
+    series: { name: string; value: number }[];
+  }[] = [{ name: '機率', series: this.series }];
+
+  public table: {
+    threshold: number;
+    comment: string;
+    probability?: number;
+    cost?: number;
+    count?: number;
+  }[] = [
+    { threshold: 0.2, comment: '神一般的人' },
+    { threshold: 0.4, comment: '運氣不錯的人' },
+    { threshold: 0.5, comment: '一般人' },
+    { threshold: 0.6, comment: '運氣較差的人' },
+    { threshold: 0.8, comment: '非洲人' },
+  ];
+
+  public view: [number, number] = [700, 400];
+
+  public ngOnInit(): void {
+    this.sameProbability = false;
+    this.awards.push({
+      name: '',
+      probability: 1,
+      goal: true,
+    });
+    this.awards.push({
+      name: '',
+      probability: 1,
+      goal: true,
+    });
+    this.awards.push({
+      name: '',
+      probability: 1,
+      goal: true,
+    });
+    this.awards.push({
+      name: '',
+      probability: 1,
+      goal: false,
+    });
+    this.awards.push({
+      name: '',
+      probability: 1,
+      goal: false,
+    });
+    this.awards.push({
+      name: '',
+      probability: 1,
+      goal: false,
+    });
+  }
+
+  public addAward() {
+    this.awards.push({
+      name: '',
+      probability: 0,
+      goal: false,
+    });
+  }
+
+  public removeAward(award: Award) {
+    const index = this.awards.indexOf(award);
+    if (index === -1) return;
+    this.awards.splice(index, 1);
+  }
+
+  public averageProbability() {
+    const probability = 100 / this.awards.length;
+    this.awards.map((award) => (award.probability = probability));
+  }
+
+  public onProbabilityChanged() {
+    const total = this.awards.reduce(
+      (prev, award) => prev + award.probability,
+      0
+    );
+
+    this.remainingProbability = 100 - total;
+  }
+
+  public async next(step: number) {
+    if (this.step < step) {
+      this.step = step;
+    }
+    await this.delay(1);
+    window.scrollTo({
+      left: 0,
+      top: document.body.clientHeight * step,
+      behavior: 'smooth',
+    });
+  }
+
+  public async calculate() {
+    this.targetAchievementProbability = 0.8;
+    this.currentAchievementProbability = 0;
+    this.needCost = 0;
+    this.needCount = 0;
+
+    this.view = [
+      document.body.clientWidth * 0.8,
+      Math.max(document.body.clientHeight - 400, 400),
+    ];
+
+    this.table.map((row) => (row.probability = 0));
+    let row = 0;
+
+    let current: Event[] = [
+      {
+        achieved: false,
+        awardIndexes: [],
+        probability: 1,
+      },
+    ];
+    let next: Event[] = [];
+
+    const awards = [...this.awards];
+
+    if (this.sameProbability) {
+      this.averageProbability();
+    } else {
+      this.onProbabilityChanged();
+      awards.push({
+        name: '其他',
+        probability: this.remainingProbability,
+        goal: false,
+      });
+    }
+
+    this.targetAwardList = awards
+      .filter((award) => award.goal)
+      .map((award) => awards.indexOf(award))
+      .sort((a, b) => a - b)
+      .join(',');
+
+    this.series = [];
+    this.results[0].series = this.series;
+
+    while (true) {
+      await this.delay(10);
+      this.needCost += this.cost;
+      this.needCount += this.quantity;
+
+      for (let i = 0; i < this.quantity; i++) {
+        await this.calculateNextEvent(current, awards, next);
+        this.mergeSameResult(next);
+        current = next;
+        next = [];
+      }
+
+      this.currentAchievementProbability = current
+        .filter((event) => event.achieved)
+        .reduce((prev, event) => prev + event.probability, 0);
+
+      this.series.push({
+        name: this.needCount.toString(),
+        value: parseFloat(
+          (this.currentAchievementProbability * 100).toFixed(2)
+        ),
+      });
+
+      if (this.currentAchievementProbability >= this.table[row].threshold) {
+        this.table[row].cost = this.needCost;
+        this.table[row].count = this.needCount;
+        this.table[row].probability = this.currentAchievementProbability;
+        row++;
+      }
+
+      if (
+        this.currentAchievementProbability >= this.targetAchievementProbability
+      ) {
+        break;
+      }
+    }
+
+    this.results = [...this.results];
+  }
+
+  private async calculateNextEvent(
+    currentRank: Event[],
+    awards: Award[],
+    nextRank: Event[]
+  ) {
+    let time = 0;
+    for (let event of currentRank) {
+      for (let awardIndex in awards) {
+        const award = awards[awardIndex];
+
+        let awardIndexes = [...event.awardIndexes];
+        let achieved = event.achieved;
+
+        if (!achieved) {
+          if (award.goal) {
+            if (awardIndexes.indexOf(parseInt(awardIndex, 10)) === -1) {
+              awardIndexes.push(parseInt(awardIndex, 10));
+              awardIndexes = awardIndexes.sort((a, b) => a - b);
+              achieved = this.targetAwardList === awardIndexes.join(',');
+            }
+          }
+        }
+
+        const nextEvent = {
+          achieved,
+          awardIndexes,
+          probability: (event.probability * award.probability) / 100,
+        };
+
+        nextRank.push(nextEvent);
+
+        if (time++ == 10) {
+          time = 0;
+          await this.delay(1);
+        }
+      }
+    }
+  }
+
+  private mergeSameResult(nextRank: Event[]) {
+    for (let i = 0; i < nextRank.length; i++) {
+      for (let j = i + 1; j < nextRank.length; j++) {
+        if (
+          nextRank[i].awardIndexes.join(',') ===
+          nextRank[j].awardIndexes.join(',')
+        ) {
+          nextRank[i].probability += nextRank[j].probability;
+          nextRank.splice(j, 1);
+          j--;
+        }
+      }
+    }
+  }
+
+  public async delay(ms: number): Promise<void> {
+    return new Promise<void>((resolve, reject) => {
+      setTimeout(() => {
+        resolve();
+      }, ms);
+    });
+  }
 }
+
+type Event = {
+  achieved: boolean;
+  probability: number;
+  awardIndexes: number[];
+};
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,35 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatTooltipModule } from '@angular/material/tooltip';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatListModule,
+    MatTooltipModule,
+    MatProgressBarModule,
+    NgxChartsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
